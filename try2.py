c1d2e3c247f81ae9d8d3b397e5f54b02329b3649
saikirann1543 <131005405+saikirann1543@users.noreply.github.com>
2023-04-19 16:12:57 +0530

    Update try2.py

--- try2.py
+++ try2.py
@@ -14,7 +14,7 @@ w2v_model = KeyedVectors.load_word2vec_format('new_another_one.bin', binary=True
 # load spacy nlp model
 nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser'])
 # Load dataset from CSV file into pandas DataFrame
-dataset = pd.read_csv("one.csv")
+dataset = pd.read_csv("one.csv", encoding="latin-1")
 # Define function to get the closest matching question from dataset using spaCy's similarity score
 def preprocess_text(text):
     # lowercase text
