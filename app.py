7736a0786b0eac6eaf2c2688bc1730d4475a871a
saikirann1543 <131005405+saikirann1543@users.noreply.github.com>
2023-04-24 17:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template, request, jsonify
 from try2 import get_dataset_response, get_category_and_rating
 import json
@@ -17,6 +18,9 @@ def predict():
         response = respo[0]
         closest_question = respo[1]
         message = {'answer': response}
+        if text.lower() in ["hi", "hello"]:
+            messages = ["Hello Doc!", "Hi Doc!"]
+            message = {'answer': random.choice(messages)}
         if text.lower() in ["end", "stop", "quit", "exit", "bye"]:
             category = get_category_and_rating(response, closest_question)
             output_string = '\n\n'.join(category)
@@ -30,45 +34,20 @@ def predict():
 
 @app.route('/rating', methods=['POST'])
 def save_rating():
-    response = {}
     data = request.json
     name = data['name']
     rating = data['rating']
-    if not os.path.exists('ratings.csv'):
-        with open('ratings.csv', mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Name', 'Rating'])
-            writer.writerow([name, rating])
-            response = {'message': 'Rating saved successfully!'}
-            print("Rating saved successfully!")
-            return jsonify(response)
-
     if os.path.exists('ratings.csv'):
         with open('ratings.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow([name, rating])
             response = {'message': 'Rating saved successfully!'}
             print("Rating saved successfully!")
-            return jsonify(response)   
+    else:
+        os.system('touch ratings.csv')
+   
     return jsonify(response)
 
-
-@app.route('/show_ratings', methods=['GET'])
-def show_ratings():
-    response = {}
-    if os.path.exists('ratings.csv'):
-        with open('ratings.csv', mode='r') as file:
-            reader = csv.reader(file)
-            response = {'ratings': list(reader)}
-    return render_template('show_ratings.html', response=response)
-
-@app.route('/show_ratings_json', methods=['GET'])
-def show_ratings_json():
-    if os.path.exists('ratings.csv'):
-        with open('ratings.csv', mode='r') as file:
-            reader = csv.reader(file)
-            response = {'ratings': list(reader)}
-    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
