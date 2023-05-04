cbcabf6fb3cb4a51d23bc7f0f2bc059ffc68f285
saikirann1543 <131005405+saikirann1543@users.noreply.github.com>
2023-05-04 23:39:15 +0530

    Update try2.py

--- try2.py
+++ try2.py
@@ -14,7 +14,7 @@ w2v_model = KeyedVectors.load_word2vec_format('Cone.bin', binary=True)
 # load spacy nlp model
 nlp = spacy.load('en_core_web_sm/', disable=['ner', 'parser'])
 # Load dataset from CSV file into pandas DataFrame
-dataset = pd.read_csv("one.csv", encoding="latin-1")
+dataset = pd.read_csv("onec.csv", encoding="latin-1")
 # Define function to get the closest matching question from dataset using spaCy's similarity score
 dataset = dataset.drop_duplicates(subset='Questions', keep='first')
 def preprocess_text(text):
@@ -25,9 +25,9 @@ def preprocess_text(text):
     text = re.sub(r'[^a-zA-Z\s]', '', text)
 
     # remove stop words
-    text = " ".join([word for word in text.split() if word not in STOP_WORDS])
+    text = " ".join([word for word in text.split() if (word not in STOP_WORDS) or (word in {'doing', 'how', 'what', 'when', 'where', 'which', 'who', 'whom', 'why', 'today'})])
 
-    # lemmatize 
+    # lemmatize text
     doc = nlp(text)
     text = " ".join([token.lemma_ for token in doc])
 
