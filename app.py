c0c53c5e61bc7ca6269e7a2d831e278aed97788b
saikirann1543 <131005405+saikirann1543@users.noreply.github.com>
2023-05-04 23:40:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,27 @@ def chatbot():
 def predict():
     try:
         text = request.get_json().get('message')
+        if text.lower() in ["end", "stop", "quit", "exit", "bye"]:
+            pass
         print("this is the text", text)
+        if text.lower() in ["hi", "hello doc!", "hey", "hi there", "hello there", "Hey there"]:
+            messages = ["Hello Doc!", "Hi Doc!", 'Hello, how are you feeling today?', 'Hi, how can I help you today?', 'Hey, how are you doing today?']
+            message = {'answer': random.choice(messages)}
+            return jsonify(json.loads(json.dumps(message, indent=4)))
         respo = get_dataset_response(text)
         response = respo[0]
         print("this is the response", response)
         closest_question = respo[1]
         message = {'answer': response}
         print("this is the messsage", message)
-        if text.lower() in ["Hi", "Hello Doc!", "Hey", "Hi there", "Hello there", "Hey there"]:
-            messages = ["Hello Doc!", "Hi Doc!", 'Hello, how are you feeling today?', 'Hi, how can I help you today?', 'Hey, how are you doing today?']
-            message = {'answer': random.choice(messages)}
         if text.lower() in ["end", "stop", "quit", "exit", "bye"]:
             category = get_category_and_rating(response, closest_question)
-            output_string = '\n\n'.join(category)
+            output_string = ' & '.join(category)
             message = {'answer': f"Thank you for chatting with me!. I hope I was able to help you with your problem. \n {output_string} "}
             return jsonify(json.loads(json.dumps(message, indent=4)))
         else:
             return jsonify(message)
+        
     except Exception as e:
         return jsonify({'answer': e})
     
@@ -53,5 +57,5 @@ def save_rating():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
                           
